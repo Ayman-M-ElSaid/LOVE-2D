a96e79529f4712fc8b8bad263eeb16567610df09
Ayman-M-ElSaid <aymanelsaid@std.mans.edu.eg>
2026-07-18 23:47:43 +0300

    refactor main.lua for improved structure and functionality; adjust score handling, and add flash effect

--- 0-Pong/main.lua
+++ 0-Pong/main.lua
@@ -1,7 +1,7 @@
-Push = require("push")
-Class = require("class")
-require("Paddle")
-require("Ball")
+Push = require("libs.push")
+Class = require("libs.class")
+local Paddle = require("src.Paddle")
+local Ball = require("src.Ball")
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -9,25 +9,26 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 PADDLE_SPEED = 200
+FLASH_DURATION = 0.15
+FLASH_INTENSITY = 5
 
 function love.load()
     math.randomseed(os.time())
 
     love.graphics.setDefaultFilter("nearest", "nearest")
+    love.window.setTitle("Pong")
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = true,
         vsync = true,
     })
-    love.window.setTitle("Pong")
     Push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = "normal" })
 
     Fonts = {
         ["large"] = love.graphics.newFont("font.ttf", 32),
-        ["medium"] = love.graphics.newFont("font.ttf", 24),
+        ["medium"] = love.graphics.newFont("font.ttf", 16),
         ["small"] = love.graphics.newFont("font.ttf", 8),
     }
-
     Sounds = {
         ["paddle_hit"] = love.audio.newSource("sounds/paddle_hit.wav", "static"),
         ["score"] = love.audio.newSource("sounds/score.wav", "static"),
@@ -37,12 +38,12 @@ function love.load()
     GameState = "start"
     ServingPlayer = math.random(2)
     Winner = nil
+    FlashTimer = 0
 
     Player1 = Paddle(15, VIRTUAL_HEIGHT / 2 - 10, 5, 20)
     Player2 = Paddle(VIRTUAL_WIDTH - 20, VIRTUAL_HEIGHT / 2 - 10, 5, 20)
-    Player1Score = 0
-    Player2Score = 0
     GameBall = Ball(VIRTUAL_WIDTH / 2, VIRTUAL_HEIGHT / 2, 4, 4)
+    Scores = { 0, 0 }
 end
 
 local function updatePlayersMovement(dt)
@@ -68,6 +69,7 @@ local function updatePlayersMovement(dt)
 end
 
 local function checkCollision()
+    -- Paddles Collision
     local player1Collision = GameBall:collides(Player1)
     local player2Collision = GameBall:collides(Player2)
 
@@ -79,55 +81,47 @@ local function checkCollision()
 
     if player1Collision then
         GameBall.x = Player1.x + Player1.width
-    end
-    if player2Collision then
+    elseif player2Collision then
         GameBall.x = Player2.x - GameBall.width
     end
 
+    --Ceiling Collisions
     if GameBall.y < 0 then
         GameBall.y = 0
         GameBall.dy = -GameBall.dy
         Sounds["wall_hit"]:play()
-    end
-    if GameBall.y > VIRTUAL_HEIGHT - GameBall.height then
+    elseif GameBall.y > VIRTUAL_HEIGHT - GameBall.height then
         GameBall.y = VIRTUAL_HEIGHT - GameBall.height
         GameBall.dy = -GameBall.dy
         Sounds["wall_hit"]:play()
     end
 end
 
-local function updateScores()   
-    if GameBall.x < -GameBall.width then
-        GameBall:reset()
-        ServingPlayer = 1
-        Player1Score = Player1Score + 1
-        Sounds["score"]:play()
-        if Player1Score == 5 then
-            GameState = "victory"
-            Winner = 1
-        else
-            GameState = "serve"
-        end
-    end
-    if GameBall.x > VIRTUAL_WIDTH then
-        GameBall:reset()
-        ServingPlayer = 2
-        Player2Score = Player2Score + 1
-        Sounds["score"]:play()
-        if Player2Score == 5 then
-            GameState = "victory"
-            Winner = 2
-        else
-            GameState = "serve"
-        end
-    end
-end
+local function updateScores()
+    local scorer
 
-local function displayFPS()
-    love.graphics.setFont(Fonts["small"])
-    love.graphics.setColor(0, 1, 0)
-    love.graphics.print(string.format("FPS: %d", love.timer.getFPS()), 10, 10)
-    love.graphics.setColor(1, 1, 1)
+    if GameBall.x < -GameBall.width then
+        scorer = 1
+        FlashX, FlashDirection = 0, 1
+    elseif GameBall.x > VIRTUAL_WIDTH then
+        scorer = 2
+        FlashX, FlashDirection = VIRTUAL_WIDTH, -1
+    end
+
+    if scorer then
+        Sounds["score"]:play()
+        Scores[scorer] = Scores[scorer] + 1
+        FlashTimer = FLASH_DURATION
+        GameBall:reset()
+        ServingPlayer = scorer
+
+        if Scores[scorer] == 5 then
+            GameState = "victory"
+            Winner = scorer
+        else
+            GameState = "serve"
+        end
+    end
 end
 
 local function renderMessages()
@@ -149,7 +143,7 @@ local function renderMessages()
         love.graphics.printf(
             string.format("Player %d wins!", Winner),
             0,
-            10,
+            15,
             VIRTUAL_WIDTH,
             "center"
         )
@@ -161,12 +155,34 @@ end
 local function renderScore()
     love.graphics.setFont(Fonts["large"])
     love.graphics.printf(
-        string.format("%d\t%d", Player1Score, Player2Score),
+        string.format("%d\t%d", Scores[1], Scores[2]),
         0,
         50,
         VIRTUAL_WIDTH,
         "center"
     )
+end
+
+local function renderFlash()
+    if FlashTimer > 0 then
+        for i = FlashX, FlashX + 10 * FlashDirection, FlashDirection / 2 do
+            love.graphics.setColor(
+            1,
+                1,
+                1,
+                FLASH_INTENSITY * FlashTimer * (1 - math.abs(i - FlashX) / 10)
+            )
+            love.graphics.rectangle("fill", i, 0, 0.5, VIRTUAL_HEIGHT)
+        end
+        love.graphics.setColor(1, 1, 1, 1)
+    end
+end
+
+local function displayFPS()
+    love.graphics.setColor(0, 1, 0)
+    love.graphics.setFont(Fonts["small"])
+    love.graphics.print(string.format("FPS: %d", love.timer.getFPS()), 10, 10)
+    love.graphics.setColor(1, 1, 1)
 end
 
 function love.keypressed(key)
@@ -178,8 +194,7 @@ function love.keypressed(key)
         elseif GameState == "serve" then
             GameState = "play"
         elseif GameState == "victory" then
-            Player1Score = 0
-            Player2Score = 0
+            Scores = { 0, 0 }
             ServingPlayer = Winner == 1 and 2 or 1
             GameState = "serve"
         end
@@ -187,8 +202,13 @@ function love.keypressed(key)
 end
 
 function love.update(dt)
+    if FlashTimer > 0 then
+        FlashTimer = FlashTimer - dt
+    end
+
     if GameState == "serve" then
-        GameBall.dx = ServingPlayer == 1 and math.random(140, 200) or -math.random(140, 200)
+        GameBall.dx = ServingPlayer == 1 and math.random(140, 200)
+            or -math.random(140, 200)
         GameBall.dy = math.random(-50, 50)
     elseif GameState == "play" then
         updatePlayersMovement(dt)
@@ -209,6 +229,7 @@ function love.draw()
 
     renderMessages()
     renderScore()
+    renderFlash()
     displayFPS()
 
     Push.finish()
@@ -217,4 +238,3 @@ end
 function love.resize(w, h)
     Push.resize(w, h)
 end
-
